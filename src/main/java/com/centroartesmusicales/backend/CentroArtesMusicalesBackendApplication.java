6f488eb128ebe7be79fdc7637c2d9c16bfb3a6fe
Francisco Mejía <pacomejia187@gmail.com>
2026-07-15 05:16:31 -0600

    Configuracion inicial del backend con Spring Boot y MySQL

---
+++ src/main/java/com/centroartesmusicales/backend/CentroArtesMusicalesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.centroartesmusicales.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroArtesMusicalesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentroArtesMusicalesBackendApplication.class, args);
+    }
+
+}
